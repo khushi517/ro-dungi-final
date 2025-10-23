d1bdad6dbbc44ca913cb28ddcd2a19df3abc7bfe
khushi517 <sssahoo517@gmail.com>
2025-10-23 19:37:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,69 @@
+// Original by 霸都丶傲天 (Oct 10, 2019)
+// Translated and adapted version
+
+var config = {
+    // You can write as many sentences as you like here.
+    // Try to keep each sentence under 15 characters for best display results.
+    texts: [
+        "For my",                      // Make sure commas are English ones!
+        "Fav hooman...", 
+        "My Dazai....",
+        "my fuljhadi...",
+        "tem to act like the main character today",
+        "cheers to another yearr with freaky ass energy",
+        "If hugs could travel through screens, you’d be squished rn 🫂",
+        "This message is 100% made of love",
+        "And all of them u match my freakiness",
+        "By the way, did you get all your gifts?",
+        "here i made this cute virtual gift for my constant human",
+        "Make sure to eat well this year",
+        "and fill your tummy~",
+        "so you’ll have energy to work",
+        "and to miss me!",
+        "Happy Birthday~~"
+    ],
+
+    /**
+     * The imgs object is optional, but if you use it,
+     * make sure the keys exactly match the corresponding texts above.
+     * Example:
+     * "beloved girl": "./imgs/love.jpg"
+     *
+     * If you don't want an image for a line, you can comment it out with //
+     * Example: the line for "Today is your birthday" has no image below.
+     * Tip: Square or nearly-square images look best.
+     */
+    imgs: {
+        "For my": "./imgs/songxinxin.jpeg",
+        "Fav hooman...": "./imgs/download (1).jpeg",
+        "My Dazai....": "./imgs/Dazai and his silly ass.jpeg",
+        "my fuljhadi...": "./imgs/Cat Art & Esthetic Edits.jpeg",
+        "tem to act like the main character today": "./imgs/Benin yaptımmmm.jpeg",
+        "cheers to another yearr with freaky ass energy": "./imgs/download (2).jpeg",
+        "If hugs could travel through screens, you’d be squished rn 🫂": "./imgs/Meowing Cats 🐈 on Instagram_ _Best friend! ♥️_.jpeg",
+        "This message is 100% made of love": "./imgs/Cat _).jpeg",
+        "And all of them u match my freakiness": "./imgs/download (2).jpeg",
+        "By the way, did you get all your gifts?": "./imgs/chuo.gif",
+        "here i made this cute virtual gift for my constant human": "./imgs/أنا وهي اتحاد كوني بين الجنون والضحك 😂💗_@safia_gamal0.jpeg",
+        "Make sure to eat well this year": "./imgs/chihaodian.jpeg",
+        "and fill your tummy~": "./imgs/weibao.jpeg",
+        "so you’ll have energy to work": "./imgs/Benin yaptımmmm.jpeg",
+        "and to miss me!": "./imgs/Cat _).jpeg",
+        "Happy Birthday~~": "./imgs/𝐶ℎ𝑢𝑢𝑦𝑎.jpeg"
+    },
+    
+    // Button text descriptions – feel free to customize these.
+    desc: {
+        turn_on: "Click to start",
+        play: "Play the music",
+        bannar_coming: "Add some color",
+        balloons_flying: "Something’s missing...",
+        cake_fadein: "Cake?",
+        light_candle: "Candles?",
+        wish_message: "Happy Birthday~",
+        story: "A MESSAGE FOR YOU"
+    },
+
+    // Ending message
+    loveText: 'Love,UR CHUYA~'
+};
